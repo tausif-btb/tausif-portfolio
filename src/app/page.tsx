e518e2f5a9aab0fa73370601cab2b067c355fef7
tausif-cil <tausif@blocktechbrew.com>
2025-04-25 17:07:20 +0530

    Comment out the projects and hackathons sections in the page component for future updates.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -111,7 +111,8 @@ export default function Page() {
 					</div>
 				</div>
 			</section>
-			<section id="projects">
+			
+			{/* <section id="projects">
 				<div className="space-y-12 w-full py-12">
 					<BlurFade delay={BLUR_FADE_DELAY * 11}>
 						<div className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -151,7 +152,8 @@ export default function Page() {
 						))}
 					</div>
 				</div>
-			</section>
+			</section> */}
+
 			{/* <section id="hackathons">
 				<div className="space-y-12 w-full py-12">
 					<BlurFade delay={BLUR_FADE_DELAY * 13}>
